f4d9f209467256bdabe1fb825a13726d4e7c7fe7
Lukas Lundmark <lukas.lundmark@foi.se>
2021-07-08 13:31:43 +0200

    Updated Feature Recon Networks

--- metric_learning/algorithms/frn.py
+++ metric_learning/algorithms/frn.py
@@ -1,48 +1,76 @@
-from typing import Optional
-from typing import Tuple
-from typing import Union
-
+from functools import cache
+from itertools import permutations
+from typing import Callable, Optional, Union
+import numpy as np
 import torch
 import torch.nn.functional as F
-from torch import cdist
-from torch import linalg as LA
+from metric_learning.algorithms.metric_learner import MetricLearner
 from torch import nn
 
 
-class FeatureReconNetwork:
+@cache
+def non_overlapping_pair_indices(n: int):
+        i1, i2 = (
+            *map(
+                torch.tensor,
+                zip(*permutations(list(range(n)), 2)),
+            ),
+        )
+        return i1, i2
+
+
+class FeatureReconNetwork(MetricLearner):
     """Feature Reconstruction Network"""
 
     def __init__(
         self,
         model: nn.Module,
-        num_channels: int,
-        dimensions: int,
-        alpha: float = 1.0,
-        beta: float = 1.0,
-        scale_factor: float = 1,
-        temperature: float = 1,
+        nchannels: int,
+        resolution: int,
+        alpha: float = 0.0,
+        beta: float = 0.0,
+        scale_factor: float = 1 / np.sqrt(640),
+        aux_loss_scale: float = 0.03,
+        temperature: float = 1.0,
+        loss_fn: Optional[Callable] = None,
+        woodbury: bool = True,
     ):
         """Feature Reconstruction Network
 
         :param model: The feature extractor (any cnn of equivelent)
-        :param num_channels: The number of output channels on the final layer of
+        :param nchannels: The number of output channels on the final layer of
             model
-        :param dimensions: Height x width of the feature maps outputed by model
+        :param resolution: Height x width of the feature maps outputed by model
         :param alpha: Initial value for learnable parameter alpha
         :param beta: Initial value for learnable parameter beta
+        :param scale_factor:
+        :param aux_loss_scale:
+        :param temperature:
+        :param loss_fn:
+        :param woodbury: Whether to use woodbury identity during feature reconstruction.
+        Recommended for larger k's
         """
 
+        super().__init__()
         self.model = model
-        self.alpha = torch.tensor(alpha)
-        self.beta = torch.tensor(beta)
 
-        self.num_channels = num_channels
-        self.dimensions = dimensions
-        self.temperature = torch.tensor(temperature)
+        # This corresponds to d in the paper
+        self.nchannels = nchannels
+        # This corresponds to r in the paper
+        self.resolution = resolution
 
+        self.alpha = nn.Parameter(torch.tensor(alpha))
+        self.beta = nn.Parameter(torch.tensor(beta))
+
+        self.temperature = nn.Parameter(torch.tensor(temperature))
         self.class_matrices: Optional[torch.Tensor] = None
         self.cached_support: Optional[torch.Tensor] = None
+
         self.scale_factor = scale_factor
+        self.aux_loss_scale = aux_loss_scale
+
+        self.criterion = nn.NLLLoss() if loss_fn is None else loss_fn
+        self.woodbury = woodbury
 
     def init_pretraining(self, num_classes: int):
         """Initialize the learner for pre-training
@@ -51,9 +79,9 @@ class FeatureReconNetwork:
         """
 
         if self.class_matrices is None or self.class_matrices.shape[0] == num_classes:
-            self.class_matrices = torch.randn(
-                (num_classes, self.dimensions, self.num_channels),
-            )
+            self.class_matrices = nn.Parameter(torch.randn(
+                (num_classes, self.resolution, self.nchannels),
+            ))
 
     def compute_support(
         self,
@@ -63,7 +91,7 @@ class FeatureReconNetwork:
         """Compute and return the class representations based on the given
         support set.
 
-        :param support: The set of support images [n,k]
+        :param support: The set of support images in format
         :param cache: Set to true to save the support representation.  This will
             make the model perform fewshot classification during the forward
             pass using the currently specified classes even if no support images
@@ -72,24 +100,60 @@ class FeatureReconNetwork:
         # Do few-shot prediction
         nway = support.shape[0]
 
-        # [nway, k*r, d]
-        support = (
-            self.model(support.flatten(0, 1))
-            .permute(0, 3, 1, 2)
-            .reshape(nway, -1, self.dimensions)
-            * self.scale_factor
-        )
+        # [nway*show, channels, ...]
+        features = self.model(support.flatten(0,1))
+
+        # nway*shots, resolution, channels
+        features = self.format_features(features)
+
+        # nway, shot*rsolution, channels
+        features = features.reshape(nway, -1, self.nchannels)
+        features *= self.scale_factor
 
         if cache:
-            self.cached_support = support
+            self.cached_support = nn.Parameter(features)
 
-        return support
+        return features
+
+    def format_features(self, features):
+        bsz = features.shape[0]
+
+        # Remember that its channel first in PyTorch
+        return features.view(bsz, self.nchannels, -1).permute(0,2,1)
+
+    def format_support(self, support, support_labels=None):
+
+        if support_labels is None:
+            if len(support.shape) > 4:
+                raise TypeError(
+                    f"Missmatched shape of support. If no support_labels are provided support needs to be shaped as [n, k, ...image_size]"
+                )
+            return support
+        else:
+            unique_support_labels, support_labels_count = support_labels.unique(
+                dim=0, return_counts=True
+            )
+
+            # FRN Requires that k is equal for all
+            assert ((support_labels_count - support_labels_count[0]) == 0).sum()
+
+            # Sort the images based on label
+            idx = torch.argsort(support_labels)
+            support = support[idx, :]
+            support = support.reshape((
+                len(unique_support_labels),
+                support_labels_count[0],
+                *support.shape[1:],),
+            )
+            return support
 
     def forward(
         self,
         query: torch.Tensor,
         support: Optional[torch.Tensor] = None,
-    ) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        support_labels: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+    ) -> tuple[torch.Tensor, ...]:
         """Predict labels using FRN.
 
         This function offerst two different modes: standard prediction and
@@ -105,7 +169,9 @@ class FeatureReconNetwork:
         prediction.
 
         :param query: Set of images such that shape=(bsz x h x w x channels)
-        :param support: Set of images such that shape=(n x k x h x w x channels)
+        :param support: Set of images such that shape=((n*k) x h x w x channels)
+        :param support_labels: Labels for the
+        :param labels: Labels for the fewshot classification task
 
         :raises ValueError: If no support images are provided and the model
             neither have a cached set of class representations nor have called
@@ -116,98 +182,133 @@ class FeatureReconNetwork:
         """
 
         # Compute and flatten the input features
-        # [bsz, r, d]
-        bsz = query.shape[0]
-        query = self.model(query).permute(0, 3, 1, 2).flatten(1, 2) * self.scale_factor
+        # [bsz, resolution, channels]
+
+        query = self.format_features(self.model(query)) * self.scale_factor
 
         if support is not None or self.cached_support is not None:
+
             if support is not None:
+                # If support is flat, convert it to the standard structure
+                support = self.format_support(support, support_labels=support_labels)
                 support = self.compute_support(support)
             else:
                 support = self.cached_support
 
             assert support is not None
-            nway = support.shape[0]
+            # nway = support.shape[0]
 
-            r = torch.exp(self.beta)
-            lam = self.num_channels / (nway * self.dimensions) * torch.exp(self.alpha)
-            recons = self._reconstruct(query, support, r, lam)
-            aux_loss = self._aux_loss(
-                recons.reshape(bsz, nway, self.dimensions, self.num_channels),
-            )
-            logits = (self._predictions(recons, query), aux_loss)
+            recons = self._reconstruct(query, support, self.alpha, self.beta)
+
+            # ways, bsz*resolution, nchannels
+            aux_loss = self._aux_loss(recons)
+
+            logits = self._predictions(recons, query)
+            return_struct = (logits,)
+
+            if labels is not None:
+                loss = self.criterion(logits, labels)
+                loss += aux_loss
+                return_struct += (loss,)
+
+            return return_struct
         else:
             # Standard predictions
             if self.class_matrices is None:
                 raise ValueError(
                     "Class matrices were not initialized, please run init_pretraining before calling method without support data",
                 )
-            recons = self._reconstruct(query, self.class_matrices, 1.0, 1.0)
+            ones = torch.ones(
+                1,
+                dtype=self.class_matrices.dtype,
+                device=self.class_matrices.device
+            )
+            recons = self._reconstruct(query, self.class_matrices, ones, ones)
             logits = self._predictions(recons, query)
+            return_struct = (logits,)
+            if labels is not None:
+                loss = self.criterion(logits, labels)
+                return_struct += (loss,)
 
-        return logits
+            return return_struct
 
     def _reconstruct(
         self,
         query: torch.Tensor,
         support: torch.Tensor,
-        r: Union[torch.Tensor, float],
-        lam: Union[torch.Tensor, float],
+        alpha: torch.Tensor,
+        beta: torch.Tensor,
     ) -> torch.Tensor:
         """Compute feature reconstructions of the query images
 
-
         :param query: [bsz, r, d]
         :param support: [way, support_shot* r, d]
-        :param r: rho
-        :param lam: lambda
+        :param alpha:
+        :param beta:
         """
-        # this code snippet is copied more or less straight from the paper's appendix
+
+        # this code snippet is copied more or less straight from the paper's appendix and Equation 8
 
         # Flatten everything
-        query = query.flatten(0, 2)
-        support = support.flatten(1, 2)
-
-        reg = support.shape[1] / support.shape[2]
+        query = query.flatten(0, 1)
+        lam = support.shape[1] / support.shape[2] * alpha.exp()
+        rho = beta.exp()
         st = support.permute(0, 2, 1)
-        xtx = st @ support
-        m_inv = (xtx + torch.eye(xtx.shape[-1]).unsqueeze(0) * (reg * lam)).inverse()
-        hat = m_inv @ xtx
 
-        # Reshape to [bsz, nway, r*d]
-        return (query @ hat) * r
+        if self.woodbury:
+            xtx = st @ support
+            eye = torch.eye(xtx.shape[-1], dtype=query.dtype, device=query.device).unsqueeze(0)
+            m_inv = (xtx + eye * lam).inverse()
+            hat = m_inv @ xtx
+        else:
+            xtx = st @ support
+            eye = torch.eye(xtx.shape[-1], dtype=query.dtype, device=query.device).unsqueeze(0)
+            m_inv = (xtx + eye * lam).inverse()
+            hat = m_inv @ xtx
 
-    def _predictions(self, recons, original):
+        # way, bsz*resoultion, nchannels
+        Q_bar = (query @ hat) * rho
+        return Q_bar
+
+    def _predictions(self, recons, query) -> torch.Tensor:
         """Computes the (normalized) logits from the reconstruction and original features
 
-        :param recons: The reconstructed features of the query set
-        :param original: The original input images
+        :param recons: The reconstructed features of the query set, shape [way, bsz*resolution, channels]
+        :param query: The query input images, [bsz, resolution, num_channels]
         """
 
-        n = recons.shape[0]
-        original = original.unsqueeze(1)
-        dists = cdist(recons, original.repeat((1, n, 1)), 2)
-        dists *= -self.temperature
-        return F.softmax(
-            dists,
-        )
+        # [1, bsz*resolution, num_channels]
+        bsz = query.shape[0]
+        query = query.flatten(0, 1).unsqueeze(0)
 
-    def _aux_loss(self, recons):
+
+        # [way, bsz*resolution, num_channels]
+        dists = recons - query
+
+        # [bsz*resolution, way]
+        dists = dists.pow(2).sum(2).permute(1, 0)
+
+        # [bsz, way]
+        dists = dists.reshape((bsz, self.resolution, -1)).mean(1)
+        dists *= -self.temperature
+        return F.log_softmax(dists, dim=-1)
+
+    def _aux_loss(self, recons) -> torch.Tensor:
         """Auxiluary loss
 
-        :param recons: [bsz, nway, dimension, channels]
+        :param recons: reconstruction of the classes [nway, bsz*resolutions, channels]
         """
 
-        def _loss(recons):
-            recons_t = recons.permute(0, 2, 1)
-            recons = recons.unsqueeze(1)
-            res = recons @ recons_t
-            nway = res.shape[0]
-            mask = (~torch.eye(nway, dtype=bool)).reshape((nway, nway, 1, 1))
+        n, batch_resolution = recons.shape[:2]
 
-            res *= mask
-            loss = LA.norm(res.flatten(0, 2), 2).sum()
-            return loss
+        # Row normalize
+        recons = recons / recons.norm(2)
 
-        loss = torch.stack([_loss(r) for r in recons]).mean()
-        return loss
+        # Compute non ovelapping indicies pairs
+        i1, i2 = non_overlapping_pair_indices(n)
+        dists = recons.index_select(0, i1) @ recons.index_select(0, i2).permute(0, 2, 1)
+        assert dists.shape[-1] == batch_resolution
+        return dists.pow(2).sum(-1).sum(-1).sum() * self.aux_loss_scale
+
+
+
